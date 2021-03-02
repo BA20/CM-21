2f14cf30016734e5123ceeb41e5e289782781d6b
Bernardo Alves <bjca1998@gmail.com>
2021-03-02 14:49:11 +0000

    hor_email

--- Kotlin_CM/app/src/main/java/ba/ipvc/kotlin_cm/MainActivity.kt
+++ Kotlin_CM/app/src/main/java/ba/ipvc/kotlin_cm/MainActivity.kt
@@ -1,11 +1,21 @@
 package ba.ipvc.kotlin_cm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun btn_toemail(view: View) {
+        val intent = Intent(this, Login_Mail::class.java).apply {
+
+        }
+        startActivity(intent)
+
+
     }
 }
