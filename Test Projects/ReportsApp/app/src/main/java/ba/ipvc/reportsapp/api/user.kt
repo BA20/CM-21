0bee8dbcbbf957d86791d353ab60372a7fcb906f
Bernardo Alves <bjca1998@gmail.com>
2021-04-29 17:54:13 +0100

    DATA CLASSES

--- Test Projects/ReportsApp/app/src/main/java/ba/ipvc/reportsapp/api/user.kt
+++ Test Projects/ReportsApp/app/src/main/java/ba/ipvc/reportsapp/api/user.kt
@@ -1,9 +1,22 @@
 package ba.ipvc.reportsapp.api
- data class User {
-     val id:
+ data class User(
+         val id:Int,
+         val email:String,
+         val password:String
 
-<<<<<<< Updated upstream
- }
-=======
-asd
->>>>>>> Stashed changes
+ )
+
+data class report(
+        val id:Int,
+        val titulo:String,
+        val descricao:String,
+        val lat:String,
+        val lng:String,
+        val user_id:Int,
+        val tipo_id:Int
+)
+
+data class tipo(
+        val id:Int,
+        val descr:String
+)
